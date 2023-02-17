19097b9ee316214822791fe2607992d04da0a983
luquilo <luquilo.etc@gmail.com>
2023-02-17 09:41:57 +0700

    tidak perlu dummy data untuk initial books array

--- buku.js
+++ buku.js
@@ -1,36 +1,11 @@
 const localStorageKey = 'books'
-
-let books = []
-const inputBookForm = document.getElementById('inputBook')
-const deleteBook1 = document.querySelector('#deleteBook')
-const deleteBook2 = document.querySelector('#deleteBook2')
-const tandaiSelesai = document.getElementById('tandaiSelesai')
-const tandaiBelumSelesai = document.getElementById('tandaiBelumSelesai')
-
-// menghapus semua local storage
-function clearLocalStorage() {
-    localStorage.clear();
+if(localStorage.getItem(localStorageKey) == null){
+    localStorage.setItem(localStorageKey, [])
 }
 
-// function menghapus buku dari rak belum selesai
-deleteBook1.addEventListener('click', function(){
-    console.log('buku dihapus!')
-})
+let books = []
 
-// function menghapus buku dari rak selesai
-deleteBook2.addEventListener('click', function(){
-    console.log('buku dihapus!')
-})
-
-// function tandai buku telah selesai
-tandaiSelesai.addEventListener('click', function(){
-    console.log('buku ditandai selesai!')
-})
-
-// function tandai buku belum selesai
-tandaiBelumSelesai.addEventListener('click', function(){
-    console.log('buku ditandai belum selesai!')
-})
+console.log(books)
 
 //mengecek apakah support local storage atau tidak
 function isStorageExist() {
@@ -38,9 +13,70 @@ function isStorageExist() {
       alert("Maaf browser anda tidak support local storage");
       return false;
     }
-  
-    return true;
 }
+
+// selector
+const inputBookForm = document.getElementById('inputBook')
+const deleteBook1 = document.querySelector('#deleteBook')
+const deleteBook2 = document.querySelector('#deleteBook2')
+const tandaiSelesai = document.getElementById('tandaiSelesai')
+const tandaiBelumSelesai = document.getElementById('tandaiBelumSelesai')
+const completeBookshelfList = document.querySelector('#completeBookshelfList')
+
+// menghapus semua local storage
+function clearLocalStorage() {
+    localStorage.clear();
+}
+
+// function menghapus buku dari rak belum selesai
+// deleteBook1.addEventListener('click', function(){
+//     console.log('buku dihapus!')
+// })
+
+// // function menghapus buku dari rak selesai
+// deleteBook2.addEventListener('click', function(){
+//     console.log('buku dihapus!')
+// })
+
+// // function tandai buku telah selesai
+// tandaiSelesai.addEventListener('click', function(){
+//     console.log('buku ditandai selesai!')
+// })
+
+// // function tandai buku belum selesai
+// tandaiBelumSelesai.addEventListener('click', function(){
+//     console.log('buku ditandai belum selesai!')
+// })
+
+
+
+// function me render buku di rak selesai
+function renderCompleteBooks(){
+    const rawLocalBook = localStorage.getItem(localStorageKey)
+    const localBook = JSON.parse(rawLocalBook)
+
+    console.log(localBook)
+    // map dari localStorage
+    const bookElements = localBook.map(book => {
+        return `
+            <article class='book_item' id='${book.id}'>
+                <h3>${book.title}</h3>
+                <p>Penulis: ${book.author}</p>
+                <p>Tahun: ${book.year}</p>
+           
+                <div class="action">
+                  <button class="green" id="tandaiSelesai">pindah rak</button>
+                  <button class="red" id="deleteBook">Hapus buku</button>
+                </div>
+            </article>
+        `
+    })
+
+    const bookListHTML = bookElements.join('')
+    return bookListHTML
+}
+
+// completeBookshelfList.innerHTML = renderCompleteBooks()
 
 // function jika form di submit
 inputBookForm.addEventListener('submit', function(event){
@@ -65,18 +101,8 @@ inputBookForm.addEventListener('submit', function(event){
     const newBook = generateBookObject()
     books.push(newBook)
 
-    console.log(books)
-
-
-    // saving books to local storage
-    function saveDataToLocalStorage() {}{
-    if(isStorageExist()){
-        const parsedBook = JSON.stringify(books)
-        localStorage.setItem(localStorageKey, parsedBook)
-    }
-
-    saveDataToLocalStorage()
-}
+    const bookStrings = localStorage.getItem('books')
+    books = JSON.parse(bookStrings)
 
 
     // membersihkan form
