744f1481a722db7935cb78ed48c308d408d8bcfb
luquilo <luquilo.etc@gmail.com>
2023-02-18 21:23:36 +0700

    rendering data based on isComplete value

--- buku.js
+++ buku.js
@@ -1,12 +1,3 @@
-const localStorageKey = 'books'
-if(localStorage.getItem(localStorageKey) == null){
-    localStorage.setItem(localStorageKey, [])
-}
-
-let books = []
-
-console.log(books)
-
 //mengecek apakah support local storage atau tidak
 function isStorageExist() {
     if (typeof Storage === undefined) {
@@ -15,23 +6,114 @@ function isStorageExist() {
     }
 }
 
-// selector
-const inputBookForm = document.getElementById('inputBook')
-const deleteBook1 = document.querySelector('#deleteBook')
-const deleteBook2 = document.querySelector('#deleteBook2')
-const tandaiSelesai = document.getElementById('tandaiSelesai')
-const tandaiBelumSelesai = document.getElementById('tandaiBelumSelesai')
-const completeBookshelfList = document.querySelector('#completeBookshelfList')
+const localStorageKey = 'books'
+const initialDummyData = [{
+    id: 1010101010,
+    title: 'judul buku',
+    author: 'author 1',
+    year: 2003,
+    isComplete: true,
+}]
+
+if(localStorage.getItem(localStorageKey) === null){
+    localStorage.setItem(localStorageKey, JSON.stringify(initialDummyData))
+}
+
+let books = JSON.parse(localStorage.getItem(localStorageKey))
+
+console.log(books)
 
 // menghapus semua local storage
 function clearLocalStorage() {
     localStorage.clear();
 }
 
-// function menghapus buku dari rak belum selesai
-// deleteBook1.addEventListener('click', function(){
-//     console.log('buku dihapus!')
-// })
+// function me render buku di rak selesai
+function renderUncompleteBooks(){
+    const rawLocalBooks = localStorage.getItem(localStorageKey)
+    const localBooks = JSON.parse(rawLocalBooks)
+
+    const filteredUncompletedBooks = localBooks.filter(book => book.isComplete === true)
+
+    console.log(localBooks)
+    console.log(filteredUncompletedBooks)
+    // map dari localStorage
+    const renderUncompletedBooks = filteredUncompletedBooks.map(book => {
+        
+        return `
+            <article class='book_item'>
+                <h3>${book.title}</h3>
+                <p>Penulis: ${book.author}</p>
+                <p>Tahun: ${book.year}</p>
+           
+                <div class="action">
+                  <button class="green ${book.id}">pindah rak</button>
+                  <button class="red" id='${book.id}'>Hapus buku</button>
+                </div>
+            </article>
+        `
+    })    
+
+    // const bookListHTML = bookElements.join('')
+    return renderUncompletedBooks
+
+}
+
+const unCompleteBookshelfList = document.querySelector('#unCompleteBookshelfList')
+unCompleteBookshelfList.innerHTML = renderUncompleteBooks()
+
+// function me render buku di rak selesai
+function renderCompleteBooks(){
+    const rawLocalBooks = localStorage.getItem(localStorageKey)
+    const localBooks = JSON.parse(rawLocalBooks)
+
+    const filteredCompletedBooks = localBooks.filter(book => book.isComplete === false)
+
+    console.log(localBooks)
+    console.log(filteredCompletedBooks)
+    // map dari localStorage
+    const renderCompletedBooks = filteredCompletedBooks.map(book => {
+        
+        return `
+            <article class='book_item'>
+                <h3>${book.title}</h3>
+                <p>Penulis: ${book.author}</p>
+                <p>Tahun: ${book.year}</p>
+           
+                <div class="action">
+                  <button class="green ${book.id}">pindah rak</button>
+                  <button class="red" id='${book.id}'>Hapus buku</button>
+                </div>
+            </article>
+        `
+    })    
+
+    // const bookListHTML = bookElements.join('')
+    return renderCompletedBooks
+
+}
+
+const completeBookshelfList = document.querySelector('#completeBookshelfList')
+completeBookshelfList.innerHTML = renderCompleteBooks()
+
+
+
+// selector
+const deleteButtons = document.querySelectorAll('.red')
+
+function removeBook(bookId){
+    console.log(bookId)
+}
+
+// function menghapus buku
+deleteButtons.forEach(button => {
+    button.addEventListener('click', function(){
+        console.log(button)
+        removeBook(button.id)
+    })
+})
+
+
 
 // // function menghapus buku dari rak selesai
 // deleteBook2.addEventListener('click', function(){
@@ -48,39 +130,11 @@ function clearLocalStorage() {
 //     console.log('buku ditandai belum selesai!')
 // })
 
-
-
-// function me render buku di rak selesai
-function renderCompleteBooks(){
-    const rawLocalBook = localStorage.getItem(localStorageKey)
-    const localBook = JSON.parse(rawLocalBook)
-
-    console.log(localBook)
-    // map dari localStorage
-    const bookElements = localBook.map(book => {
-        return `
-            <article class='book_item' id='${book.id}'>
-                <h3>${book.title}</h3>
-                <p>Penulis: ${book.author}</p>
-                <p>Tahun: ${book.year}</p>
-           
-                <div class="action">
-                  <button class="green" id="tandaiSelesai">pindah rak</button>
-                  <button class="red" id="deleteBook">Hapus buku</button>
-                </div>
-            </article>
-        `
-    })
-
-    const bookListHTML = bookElements.join('')
-    return bookListHTML
-}
-
-// completeBookshelfList.innerHTML = renderCompleteBooks()
-
+// form selector
+const inputBookForm = document.getElementById('inputBook')
 // function jika form di submit
 inputBookForm.addEventListener('submit', function(event){
-    event.preventDefault();   
+    // event.preventDefault();   
 
     const id = +new Date();
     const title = document.getElementById('inputBookTitle').value
@@ -101,15 +155,18 @@ inputBookForm.addEventListener('submit', function(event){
     const newBook = generateBookObject()
     books.push(newBook)
 
-    const bookStrings = localStorage.getItem('books')
-    books = JSON.parse(bookStrings)
+    localStorage.setItem(localStorageKey, JSON.stringify(books))
+    
+    // updating the books array berdasarkan local storage
+    books = JSON.parse(localStorage.getItem(localStorageKey))
+    console.log(books)
 
 
-    // membersihkan form
-    inputBookTitle.value = ''
-    inputBookAuthor.value = ''
-    inputBookYear.value = ''
-    inputBookIsComplete.checked = false 
+    // // membersihkan form
+    // inputBookTitle.value = ''
+    // inputBookAuthor.value = ''
+    // inputBookYear.value = ''
+    // inputBookIsComplete.checked = false 
 
 
 
@@ -117,9 +174,9 @@ inputBookForm.addEventListener('submit', function(event){
 
     // alert terimakasih
     alert('terimakasih telah mengisi formulir!')
-
-    // console.log(`${id} ${judul} ${penulis} ${tahun} ${isComplete}`)
 })
+
+
 
 
 
